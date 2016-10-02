61da0b43493b531eacf8c99e2504c0bf282e7c2d
goodmj <matthew.j.good@gmail.com>
2016-10-02 21:01:04 +0000

    First file (a Pi blog entry stored into Github source control. Woot woot!

---
+++ python/card.py
@@ -0,0 +1,76 @@
+def display():
+    print('Hello World!')
+    s = 'Hello World 2!'
+    str(s)
+
+display()
+
+# class Score
+# class Scores
+
+class Card:
+##    category = "testCategory"
+##    text = "foo text"
+##    # scores = "" # array of scoreAndDates ** *
+    # front = "foo" #
+    # back = "bar" #
+    # lastShown = "9/1/16" #
+    # frontText = "" #
+    # frontLink = "" #
+    # link = "" #
+    # format = "prototype" #
+            
+    def display():
+        print('Hello World!')
+        s = 'Hello World 2!'
+        str(s)
+        
+        #   format = "pretotype"
+##        if format == "pretotype"
+##            print ""
+##            print "###########"
+##            print "Category: Pretotype Category"
+##            print "###########**"
+##            print "FrontText: Hello"
+##            print "###########"
+##            print "BackTest: World
+##            print "###* End Cart ###*"
+##            print "###########"
+##            print ""
+##        if (format= "prototype")
+##            print ""
+##            print "#########**"
+##            print "Category: Prototype Category"
+##            print "#########**"
+##            print "FrontText: Hello"
+##            print "#########"
+##            print "BackTest: World
+##            print "###* End Cart ###*"
+##            print "#########**"
+##            print ""#
+
+    # Thread to load links, pics, etc. #
+##    def start(format)
+##        # if (format = pretotype),
+##
+##    def fetchData
+##        return "sampleData"
+##    
+##    def f(self):
+##        return Content
+
+##    def loadLink
+##    def loadText
+##    def loadExternalResource
+##    def displayLink
+##    def displayText
+##    def loadContent
+##    def displayContent
+##    def displayCategory
+##    def displayUI #BAD, TEMPORARY** (or is it?) #
+##    def receiveUserInput
+##    def displayResults
+##    def storeResults
+
+x = Card()
+x.display
---
+++ python/deck.py
@@ -0,0 +1,81 @@
+# class Score
+# class Scores
+
+class Deck:
+    #cards = <Array or List/Queue>
+    name = "DeckSampleDataText Name"
+    #filename= ".\SampleDeckData.txt"
+    cards = []
+
+    def parseFile(this.filename)
+    {
+        results = File.open(filename)
+        return results
+    }
+
+    # scores = "" # array of scoreAndDates ** *
+    # front = "foo" #
+    # back = "bar" #
+    # lastShown = "9/1/16" #
+    # frontText = "" #
+    # frontLink = "" #
+    # link = "" #
+    # format = "prototype" #
+    
+    def display()
+        Print "Hello World!"
+        
+##    def display(format)
+##        format.type = "pretotype"
+##        if (format = pretotype)
+##            print ""
+##            print "#########**"
+##            print "Category: Pretotype Category"
+##            print "#########**"
+##            print "FrontText: Hello"
+##            print ""#########**""
+##            print "BackTest: World
+##            print "###* End Cart ###*"
+##            print "#########**"
+##            print ""
+##        if (format= "prototype")
+##            print ""
+##            print "#########**"
+##            print "Category: Prototype Category"
+##            print "#########**"
+##            print "FrontText: Hello"
+##            print ""#########**""
+##            print "BackTest: World
+##            print "###* End Cart ###*"
+##            print "#########**"
+##            print ""
+
+    # Thread to load links, pics, etc. #
+    def start(format)
+        # if (format = pretotype)
+
+    def fetchData
+        return "sampleData"
+    
+    def f(self):
+        return Content
+
+    def loadLink
+    def loadText
+    def loadExternalResource
+    def displayLink
+    def displayText
+    def loadContent
+    def displayContent
+    def displayCategory
+    def displayUI #BAD, TEMPORARY** (or is it?) #
+    def receiveUserInput
+    def displayResults
+    def storeResults
+    
+
+    def display()
+        print "Hello World!"
+
+class main
+    Card.Start
